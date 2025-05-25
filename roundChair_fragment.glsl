15005409c9389995409e67c288b0a6e771af1419
Angelique Breedt <u23542838@tuks.co.za>
2025-05-25 18:32:11 +0200

    Added round yellow chairs

---
+++ roundChair_fragment.glsl
@@ -0,0 +1,34 @@
+#version 330 core
+
+out vec4 FragColor;
+
+in vec3 FragPos;
+in vec3 Normal;
+
+uniform vec3 color;
+
+void main()
+{
+    // simple lighting calc.
+    vec3 lightPos = vec3(5.0, 8.0, 5.0);
+    vec3 lightColor = vec3(1.0, 1.0, 1.0);
+    vec3 ambientColor = vec3(0.3, 0.3, 0.3);
+    
+    // ambient
+    vec3 ambient = ambientColor * color;
+    
+    // diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor * color;
+    
+    // specular
+    vec3 viewDir = normalize(-FragPos); // Camera is at origin
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = spec * lightColor * 0.5;
+    
+    vec3 result = ambient + diffuse + specular;
+    FragColor = vec4(result, 1.0);
+}
