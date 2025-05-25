bc7b11f808b277b9d29c3567567a1cfc9673513e
Angelique Breedt <u23542838@tuks.co.za>
2025-05-25 15:44:04 +0200

    Added POINT LIGHT fragment_shader.glsl

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -1,42 +1,67 @@
 #version 330 core
+
+struct PointLight {
+    vec3 position;
+    vec3 color;
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+#define NR_POINT_LIGHTS 4  // match this w. the no, of lights
+
 in vec3 FragPos;
 in vec3 Normal;
 in vec2 TexCoord;
 
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
+uniform PointLight pointLights[NR_POINT_LIGHTS];
 uniform vec3 viewPos;
-
-uniform sampler2D texture1;
+uniform vec3 objectColor;
 uniform bool useTexture;
 uniform float alpha;
+uniform sampler2D texture1;
 
 out vec4 FragColor;
 
+vec3 CalculatePointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+
 void main()
 {
-    // Ambient
-    float ambientStrength = 0.2;
-    vec3 ambient = ambientStrength * lightColor;
-
-    // Diffuse
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-
-    // Specular
-    float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
+    vec3 result = vec3(0.0);
+    
+    // calc all point lights
+    for(int i = 0; i < NR_POINT_LIGHTS; i++)
+        result += CalculatePointLight(pointLights[i], norm, FragPos, viewDir);
+    
+    // ambient contribution
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * objectColor;
+    result += ambient;
 
-    // Combine lighting
-    vec3 lighting = (ambient + diffuse + specular);
-
-    // Final color
+    // combine w. texture
     vec3 baseColor = useTexture ? texture(texture1, TexCoord).rgb : objectColor;
-    FragColor = vec4(baseColor * lighting, alpha);
+    FragColor = vec4(baseColor * result, alpha);
+}
+
+vec3 CalculatePointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+    vec3 lightDir = normalize(light.position - fragPos);
+    
+    // diffuse
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 diffuse = diff * light.color;
+    
+    // specular
+    float specularStrength = 0.5;
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * light.color;
+    
+    // attenuation
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+    
+    return (diffuse + specular) * attenuation;
 }
